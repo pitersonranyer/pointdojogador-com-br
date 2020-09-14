97b5b450e6e9cd4cb06e149a61c9d0b97c84a1d5
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 15:12:30 -0300

    save umbler 14/09

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const path = require('path');
-const nomeApp = process.env.npm_package_name;
-const app = express();
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
  
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static(path.join(__dirname, 'dist', 'pointdojogador-ui'
+)))
  
-app.listen(process.env.PORT || 3000);
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', 'pointdojogador-ui', 'index.html'))
+})
+ 
+app.listen(port)
