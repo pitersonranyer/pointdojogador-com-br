210c1b3b356281fc38ed6f5a7e39cc1a0de4390e
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 14:28:19 -0300

    save umbler 14/09

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const path = require('path');
-const nomeApp = process.env.npm_package_name;
-const app = express();
- 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+var express = require('express');
+var app = express();
+
+//... your code here ...
+                                
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
 });
- 
-app.listen(process.env.PORT || 3000);
