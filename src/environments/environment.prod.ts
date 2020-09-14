fd463fbf9fd49721021cd0c8ac05b2dda5d30780
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 15:17:51 -0300

    save umbler 14/09

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  pointdojogadorApiUrl: 'https://pointdojogador-api.herokuapp.com/api',
 };
+
+export const TOKEN_STORAGE = 'NOME_CHAVE_TOKEN';
+export const TOKEN_GLOBO = '';
