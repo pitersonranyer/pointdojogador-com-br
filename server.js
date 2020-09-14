124e64a25280ec20648c6c7db64eb5c63414f50f
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 15:00:07 -0300

    save umbler 14/09

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
 });
  
 app.listen(process.env.PORT || 3000);
