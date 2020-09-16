8f93b2b14cdbc990b3713c374461dc62c6d6d9e0
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-16 18:41:11 -0300

    ajuste chamada backend 5.

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 3000
-const app = express()
- 
-app.use(express.static(path.join(__dirname, 'dist', 'pointdojogador-ui'
-)))
- 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'dist', 'pointdojogador-ui', 'index.html'))
-})
- 
-app.listen(port)
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const configs = {
+    caminho: "dist/pointdojogador-ui", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos. Troque "myapp" pelo nome da sua aplicação
+    forcarHTTPS: false, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
+    port: process.env.PORT || 3000
+}
+
+if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
+    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+        if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+            res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+        else //Se a requisição já é HTTPS
+            next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+    });
+
+app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+
+app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
+    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+});
+
+app.listen(configs.port, () => {
+    console.log(`Escutando na ${configs.port}!`);
+});
