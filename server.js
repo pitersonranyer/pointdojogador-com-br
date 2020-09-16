65b7fafb02e2d94134ff94de0f5551343fa35bf4
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-16 17:30:15 -0300

    ajuste chamada backend 2.

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 3000
-const app = express()
- 
-app.use(express.static(path.join(__dirname, 'dist', 'pointdojogador-ui'
-)))
- 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'dist', 'pointdojogador-ui', 'index.html'))
-})
- 
-app.listen(port)
+var express = require('express');
+var app = express();
+
+//... your code here ...
+                                
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
