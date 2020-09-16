bd3005eda58d8d135114f07817a36513c24ae4a9
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-15 23:09:13 -0300

    save umbler 15/09

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-var path = require('path');
-var express = require('express');
-var app = express();
- 
-app.set('port', (process.env.PORT || 3000));
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
  
 app.use(express.static(path.join(__dirname, 'dist', 'pointdojogador-ui'
 )))
  
-// redireciona todas as requições para o Angular 2 
-app.all('*', function(req, res) {
-  res.status(200).sendFile(
-   path.join(__dirname, 'dist', 'pointdojogador-ui', 'index.html'));
-});
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', 'pointdojogador-ui', 'index.html'))
+})
  
-app.listen(app.get('port'), function() {
-  console.log('Node executando na porta ', app.get('port'));
-});
+app.listen(port)
