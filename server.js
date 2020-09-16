de095c559d38c22829500dde57250f8cace5df60
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-15 22:59:54 -0300

    save umbler 15/09

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-var secure = require('express-force-https');
  
 app.set('port', (process.env.PORT || 3000));
  
-app.use(secure);
 app.use(express.static(path.join(__dirname, 'dist')));
  
 // redireciona todas as requições para o Angular 2 
