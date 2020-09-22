42532e4fd95e86cd201c8d20b994c0d8fc1b46fc
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-21 23:01:21 -0300

    save 21/09 api

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -16,7 +16,6 @@ export class UtilService {
         }
         if (url.match(/pointdojogador/)) {
             return 'https://api.pointdojogador.com.br/api';
-           // return 'http://pointdojogador-api-com-br.umbler.net/api';
         }
     }
 
