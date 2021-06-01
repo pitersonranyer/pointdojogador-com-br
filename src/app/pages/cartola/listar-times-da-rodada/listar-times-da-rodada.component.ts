543ff7197e07521b10708f355353c829b34a8957
Piterson Ranyer <pitersonranyer@gmal.comr>
2021-06-01 15:53:30 -0300

    ajustes

--- src/app/pages/cartola/listar-times-da-rodada/listar-times-da-rodada.component.ts
+++ src/app/pages/cartola/listar-times-da-rodada/listar-times-da-rodada.component.ts
@@ -39,6 +39,7 @@ export class ListarTimesDaRodadaComponent implements OnInit {
   public valorRodada = 0;
   public totalParticipantes = 0;
   public podeAtualizar = false;
+  public tipoCompeticao = '';
 
   public timeRodadaCartola: TimeRodadaCartola = <TimeRodadaCartola>{};
 
@@ -65,12 +66,13 @@ export class ListarTimesDaRodadaComponent implements OnInit {
       this.nrSequencialRodadaCartola = params.nrSequencialRodadaCartola;
       this.nrRodada = params.nrRodada;
       this.valorRodada = params.valorCompeticao;
+      this.tipoCompeticao = params.tipoCompeticao;
 
       this.consultarMercadoStatus.consultarMercadoStatus().subscribe(status => {
         if ((status.rodada_atual - 1) == this.nrRodada) {
           this.podeAtualizar = true;
         }
-        if  (this.nrRodada == 0 ){
+        if (this.nrRodada == 0) {
           this.podeAtualizar = true;
         }
       });
@@ -85,43 +87,43 @@ export class ListarTimesDaRodadaComponent implements OnInit {
     this.listarTimesDaCompeticaoService.listarTimesDaCompeticao(this.nrSequencialRodadaCartola)
       .subscribe((resultParcial: any[]) => {
         this.parciais = resultParcial;
-
         this.totalParticipantes = this.parciais.length;
 
         this.premiacaoTotal = this.totalParticipantes * this.valorRodada;
-
-        for (let j = 0; j < this.parciais.length; j++) {
-          this.premiacaoPercentualLista = 0;
-          this.premiacaoFinalLista = 0;
-          this.parciais[j].premiacaoFinalFormatLista = 0;
-          if (j === 0) {
-            this.premiacaoPercentualLista = (this.premiacaoTotal * 50) / 100;
-            this.premiacaoFinalLista = this.premiacaoPercentualLista;
-            this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
-          }
-          if (j === 1) {
-            this.premiacaoPercentualLista = (this.premiacaoTotal * 25) / 100;
-            this.premiacaoFinalLista = this.premiacaoPercentualLista;
-            this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
-          }
-          if (j === 2) {
-            this.premiacaoPercentualLista = (this.premiacaoTotal * 10) / 100;
-            this.premiacaoFinalLista = this.premiacaoPercentualLista;
-            this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
-          }
-          if (j === 3) {
-            this.premiacaoPercentualLista = (this.premiacaoTotal * 5) / 100;
-            this.premiacaoFinalLista = this.premiacaoPercentualLista;
-            this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
-          }
-          if (j === 4) {
-            this.parciais[j].premiacaoFinalFormatLista = '10,00';
-          }
-          if (j === 5) {
-            this.parciais[j].premiacaoFinalFormatLista = '10,00';
-          }
-          if (j === 6) {
-            this.parciais[j].premiacaoFinalFormatLista = '10,00';
+        if (this.tipoCompeticao === 'TIRO CURTO') {
+          for (let j = 0; j < this.parciais.length; j++) {
+            this.premiacaoPercentualLista = 0;
+            this.premiacaoFinalLista = 0;
+            this.parciais[j].premiacaoFinalFormatLista = 0;
+            if (j === 0) {
+              this.premiacaoPercentualLista = (this.premiacaoTotal * 50) / 100;
+              this.premiacaoFinalLista = this.premiacaoPercentualLista;
+              this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+            }
+            if (j === 1) {
+              this.premiacaoPercentualLista = (this.premiacaoTotal * 25) / 100;
+              this.premiacaoFinalLista = this.premiacaoPercentualLista;
+              this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+            }
+            if (j === 2) {
+              this.premiacaoPercentualLista = (this.premiacaoTotal * 10) / 100;
+              this.premiacaoFinalLista = this.premiacaoPercentualLista;
+              this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+            }
+            if (j === 3) {
+              this.premiacaoPercentualLista = (this.premiacaoTotal * 5) / 100;
+              this.premiacaoFinalLista = this.premiacaoPercentualLista;
+              this.parciais[j].premiacaoFinalFormatLista = this.premiacaoFinalLista.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+            }
+            if (j === 4) {
+              this.parciais[j].premiacaoFinalFormatLista = '10,00';
+            }
+            if (j === 5) {
+              this.parciais[j].premiacaoFinalFormatLista = '10,00';
+            }
+            if (j === 6) {
+              this.parciais[j].premiacaoFinalFormatLista = '10,00';
+            }
           }
         }
       });
@@ -136,42 +138,28 @@ export class ListarTimesDaRodadaComponent implements OnInit {
       this.consultarTimeCartola.consultarTimeCartola(this.parciais[i].time_id)
         .subscribe((data) => {
 
-          this.capitao_id = data.capitao_id;
-
           this.totPontos = 0;
           this.pontuacaoParcial = 0;
-          for (let x = 0; x < data.atletas.length; x++) {
-            // Dobrar pontuação do capitão
-            if (this.capitao_id == data.atletas[x].atleta_id) {
-              this.pontuacaoParcial = data.atletas[x].pontos_num * 2;
-            } else {
-              this.pontuacaoParcial = data.atletas[x].pontos_num;
-            }
-            this.totPontos += this.pontuacaoParcial;
-          }
-
-
-          
 
           // Atualizar pontuação. (piterson)
           this.timeBilhete.idBilhete = this.parciais[i].idBilhete
           this.timeBilhete.time_id = this.parciais[i].time_id;
-          this.timeBilhete.pontuacaoParcial = this.totPontos;
+          this.timeBilhete.pontuacaoParcial = data.pontos;
           this.timeBilhete.pontuacaoParcial.toFixed(2);
           this.timeBilhete.qtJogadoresPontuados = 12; //ajustar;
           this.timeBilhete.pontuacaoTotalCompeticao = data.pontos_campeonato;
           this.timeBilhete.pontuacaoTotalCompeticao.toFixed(2);
 
           this.atualizarResultadoParcial.atualizarPontosTimeBilhete(this.timeBilhete)
-           .subscribe(() => {
-            this.pontuacaoTimeRodada.time_id = this.parciais[i].time_id;
-            this.pontuacaoTimeRodada.nrRodada = this.nrRodada;
-            this.pontuacaoTimeRodada.pontuacao = this.totPontos;
-      
-            this.atualizarPontuacaoService.atualizarPontuacaoTimeRodada(this.pontuacaoTimeRodada)
-              .subscribe(() => {
-              });
-              
+            .subscribe(() => {
+              this.pontuacaoTimeRodada.time_id = this.parciais[i].time_id;
+              this.pontuacaoTimeRodada.nrRodada = this.nrRodada;
+              this.pontuacaoTimeRodada.pontuacao = data.pontos;
+
+              this.atualizarPontuacaoService.atualizarPontuacaoTimeRodada(this.pontuacaoTimeRodada)
+                .subscribe(() => {
+                });
+
             });
         });
 
